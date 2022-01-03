b92a68138810095972c672df4175128d608d6d19
Joan Bondri <joan.bondri@gmail.com>
2022-01-03 09:32:39 +0100

    prompt

---
+++ src/get_prompt.c
@@ -0,0 +1,6 @@
+#include "minishell.h"
+
+char	*get_prompt(void)
+{
+	return (ft_strdup("prompt >"));
+}
--- src/parse.c
+++ src/parse.c
@@ -3,9 +3,15 @@
 t_data	parse(int ac, char **av, char **env)
 {
 	t_data	*data;
+	char	*str;
 
 	data = malloc(sizeof(t_data) * 1);
 	if (!data)
 		return (NULL);
 	data->env = list_env(env);
+	while (true)
+	{
+		str = readline(get_prompt());
+		deconstruct(str);
+	}
 }
