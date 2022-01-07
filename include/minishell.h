c64acb1aec1487985639c0675d43cb33df4ff26a
Joan Bondri <joan.bondri@gmail.com>
2022-01-07 12:32:14 +0100

    riwe

--- include/minishell.h
+++ include/minishell.h
@@ -6,40 +6,45 @@
 #include <stdio.h>
 #include <readline/readline.h>
 #include <readline/history.h>
-#include "parse.h"
+# include "parse.h"
 
 typedef struct s_cmd
 {
-    char			*path;
-    char			**arg;
-    char			**env;
+    char *path;
+    char **arg;
+    // char ***env;
+    char    *path_infile;
+    int     infile;
+    char    *path_outfile;
+    int     outfile;
     struct s_cmd	*next;
-	struct s_cmd	*prev;
+    struct s_cmd	*prev;
 }               t_cmd;
 
 typedef struct s_env
 {
-    char   			*var;
-    char   			**val;
-    struct s_env	*next;
-    struct s_env	*prev;
+    char    *var;
+    char    **val;
+    struct s_env   *next;
+    struct s_env   *prev;
 }               t_env;
 
 typedef struct s_data
 {
     int     here_doc;
-    char    *here_doc;
-    int     input;
-    char    *input;
-    int     output;
+    char    *delimiter;
+    int     infile;
+    char    *path_infile;
+    int     outfile;
     int     append_mode;
-    char    *output;
+    char    *path_outfile;
     int     nbr_pipe;
     int     pipe_even[2];
     int     pipe_odd[2];
+    char    ***env_strs;
     int     nbr_cmd;
-    t_cmd   *cmd;
-    t_env   *env;
+    t_cmd   **cmd;
+    t_env   **env;
 }               t_data;
 
 void	exec_data(t_data data);
