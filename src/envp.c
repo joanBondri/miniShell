ba4cfaf24c06d97476d1dd7508225ef80e2f1419
Joan Bondri <joan.bondri@gmail.com>
2022-01-11 14:49:13 +0100

    add envp

--- src/envp.c
+++ src/envp.c
@@ -7,6 +7,8 @@ char	*get_env(char *var, t_env *lst)
 	buff = match_lstenv(var, lst);
 	return (buff->val[0]);
 }
+
+//tu mets en la variable et la liste
 t_env	*match_lstenv(char *var, t_env *lst)
 {
 	t_env	*buff;
@@ -38,16 +40,17 @@ void	*ft_putlst_end_env(char *str, t_env **lst)
 	if (!lst)
 		return (NULL);
 	res = malloc(sizeof(t_env));
-	if (!tbs || !res)
+	if (!res)
 		return (NULL);
-	*res = {};
-	tbs = ft_split_mnshell(str, "=");
+	*res = (t_env){0};
+	tbs = ft_first_split(str, "=");
 	res->var = tbs[0];
 	if (tbs[1] && tbs[1][0] == ':')
 		res->val = tbs + 1;
 	else
 	{
-		res->val = ft_split_mnshell(tbs[1], ":");
+		//a revoir pour le cas ou l' argument se finit par un :
+		res->val = ft_split(tbs[1], ":");
 		if (tbs[1] && !(res->val))
 		{
 			free(res);
@@ -66,15 +69,19 @@ void	*ft_putlst_end_env(char *str, t_env **lst)
 	return (res);
 }
 
-t_env	*list_env(char **var)
+//fonction principale, tu lui fout les env
+t_env	**list_env(char **var)
 {
-	t_env	*res;
+	t_env	**res;
 
 	--var;
-	res = NULL;
+	res = malloc(sizeof(t_env*));
+	if (!res)
+		return (NULL);
+	*res = NULL;
 	while (*(++var))
 	{
-		if (!ft_putlst_end_env(*var, &res))
+		if (!ft_putlst_end_env(*var, res))
 			return (NULL);
 	}
 	return (res);
