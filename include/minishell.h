b5a6e40812a412759714ccf327ddf8340883ccaf
Joan Bondri <joan.bondri@gmail.com>
2022-02-04 17:27:16 +0100

    je suis laaaa

--- include/minishell.h
+++ include/minishell.h
@@ -12,35 +12,20 @@ typedef struct s_cmd
 {
     char *path;
     char **arg;
-    char    *path_infile;
     int     infile;
-    char    *path_outfile;
     int     outfile;
     struct s_cmd	*next;
     struct s_cmd	*prev;
 }               t_cmd;
 
-typedef struct s_env
-{
-    char    *var;
-    char    **val;
-    struct s_env	*next;
-    struct s_env	*prev;
-}               t_env;
-
 typedef struct s_data
 {
     int     here_doc;
     char    *delimiter;
-    int     infile;
-    char    *path_infile;
-    int     outfile;
-    int     append_mode;
-    char    *path_outfile;
     int     nbr_pipe;
     int     pipe_even[2];
     int     pipe_odd[2];
-    char    ***env;
+    char    **env;
     int     nbr_cmd;
     t_cmd   **cmd;
 }               t_data;
