189989e9db85552239b7bff8e58ec4439349d543
Xavier CHALLE <xchalle@e2r6p5.clusters.42paris.fr>
2022-02-13 22:00:41 +0100

    ok

--- libft/ft_strcmp.c
+++ libft/ft_strcmp.c
@@ -5,6 +5,8 @@ int	ft_strcmp(const char *s1, const char *s2)
 	unsigned char	c1;
 	unsigned char	c2;
 
+	if (!s1 || !s2)
+		return (1);
 	c1 = (unsigned char)*s1;
 	c2 = (unsigned char)*s2;
 	while (*s1 && *s2)
